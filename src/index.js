40a2cecaa60faa671c3fb1e79e8ab7bba68bbc5c
Matías <matiasagbenitez@gmail.com>
2022-11-30 23:53:14 -0300

    Alerta chistosa

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
+    {/* Alerta por única vez */}
+    <div className="alert alert-warning alert-dismissible fade show" role="alert">
+      <strong>¡Atención!</strong> Este sitio es aún muy sensible. Por favor, no romper nada. Si desea poner a prueba validaciones hágalo en otro sitio. Gracias.
+      <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
     <App />
   </React.StrictMode>
 );
